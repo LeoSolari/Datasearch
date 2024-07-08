ed11185b02cc090ba952ea04964547a104b52058
Leandro Solari <leosolari9@gmail.com>
2024-07-08 11:44:41 -0300

    Search pick id

--- front/app/openWorks/picks/page.jsx
+++ front/app/openWorks/picks/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPicks } from "@/redux/slices/pickSlice";
 import Link from 'next/link';
@@ -8,10 +8,19 @@ import Link from 'next/link';
 const PicksComponent = () => {
   const dispatch = useDispatch();
   const { picks, status, error } = useSelector((state) => state.picks);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     dispatch(fetchPicks());
   }, [dispatch]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPicks = picks.filter(pick =>
+    pick.PICK_SURF_ID.toString().includes(searchTerm)
+  );
 
   if (status === "loading") {
     return <div>Loading...</div>;
@@ -24,13 +33,23 @@ const PicksComponent = () => {
   return (
     <div className="p-24">
       <h1>All Picks</h1>
+      <div className="mb-4">
+        <label htmlFor="search">Search by PICK SURF ID:</label>
+        <input
+          type="text"
+          id="search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className="ml-2 px-2 py-1 border border-gray-300 rounded"
+        />
+      </div>
       <ul>
-        {picks.map((pick, i) => (
+        {filteredPicks.map((pick, i) => (
           <li key={i} className="p-4">
             <Link href={`/openWorks/picks/${pick.WELL_ID}`}>
               <div className="text-blue-500">
                 <p>WELL_ID: {pick.WELL_ID}</p>
-                <p>Name: {pick.PICK_SURF_ID}</p>
+                <p>PICK SURF ID: {pick.PICK_SURF_ID}</p>
               </div>
             </Link>
           </li>
